3b71feea6cfca429dae3e0fe95a38c26824e6dae
BeLeap <beleap@beleap.dev>
2024-04-21 20:57:13 +0900

    feat: add dummy handler

--- pkg/teleproxy/proxy.go
+++ pkg/teleproxy/proxy.go
@@ -6,9 +6,20 @@ import (
 	"net/http"
 )
 
+var (
+	_ http.Handler = &proxyHandler{}
+)
+
+type proxyHandler struct{}
+
+func (p *proxyHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+	log.Print("request recv")
+}
+
 func StartProxy(port int) {
-	s := &http.Server {
+	s := &http.Server{
 		Addr: fmt.Sprintf(":%d", port),
+		Handler: &proxyHandler {},
 	}
 	log.Fatal(s.ListenAndServe())
 }
