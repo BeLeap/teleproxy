98bb6c44397b5909898694b8fb6c1a6e10efa936
BeLeap <beleap@beleap.dev>
2024-04-21 20:06:30 +0900

    init: cobra cmd

--- cmd/teleproxy/main.go
+++ cmd/teleproxy/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command {
+	Use: "Teleproxy",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, World!")
+	},
+}
+
+var cfgFile string
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "path for config file")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err);
+		os.Exit(1);
+	}
 }
