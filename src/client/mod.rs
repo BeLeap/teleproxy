ed84e5519ea8d0cc4cc65a057a4f96a9bb1eb7b8
BeLeap <beleap@beleap.dev>
2024-07-07 13:37:22 +0900

    check phase on listen

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 
-use crate::{dto::phase::ListenPhase, proto};
+use crate::{
+    dto::phase::ListenPhase,
+    proto,
+};
 
 type Client = proto::teleproxy::teleproxy_client::TeleproxyClient<tonic::transport::Channel>;
 
@@ -80,57 +83,64 @@ pub async fn listen(
     while let Some(result) = out_stream.next().await {
         let listen_response = result.unwrap();
 
-        let client = reqwest::Client::new();
+        match listen_response.phase.try_into().unwrap() {
+            ListenPhase::Tunneling => {
+                let client = reqwest::Client::new();
 
-        let method: http::Method = match listen_response.method.parse() {
-            Ok(v) => v,
-            Err(_err) => {
-                todo!()
+                let method: http::Method = match listen_response.method.parse() {
+                    Ok(v) => v,
+                    Err(_err) => {
+                        todo!()
+                    }
+                };
+                let url = target.parse::<reqwest::Url>().unwrap();
+                let client = client.request(method, url);
+
+                let mut headers = reqwest::header::HeaderMap::new();
+                for header in listen_response.headers {
+                    let key = reqwest::header::HeaderName::from_bytes(header.0.as_bytes()).unwrap();
+                    headers.insert(key, header.1.parse().unwrap());
+                }
+                let client = client.headers(headers);
+
+                let client = client.body(listen_response.body);
+                let http_response = client.send().await;
+
+                let mut http_response = match http_response {
+                    Ok(v) => v,
+                    Err(_) => todo!(),
+                };
+
+                let status_code = http_response.status().as_u16() as i32;
+
+                let headers = http_response.headers_mut();
+                let headers = headers
+                    .iter_mut()
+                    .map(|header| (header.0.to_string(), header.1.to_str().unwrap().to_string()))
+                    .collect();
+
+                let body = http_response.bytes().await;
+                let body = match body {
+                    Ok(v) => v,
+                    Err(_) => todo!(),
+                };
+                let body = body.to_vec();
+
+                let listen_request = proto::teleproxy::ListenRequest {
+                    api_key: "".to_string(),
+                    id: id.to_string(),
+                    phase: ListenPhase::Tunneling as i32,
+                    status_code,
+                    headers,
+                    body,
+                };
+
+                let _ = stream_tx.send(listen_request).await;
             }
-        };
-        let url = target.parse::<reqwest::Url>().unwrap();
-        let client = client.request(method, url);
-
-        let mut headers = reqwest::header::HeaderMap::new();
-        for header in listen_response.headers {
-            let key = reqwest::header::HeaderName::from_bytes(header.0.as_bytes()).unwrap();
-            headers.insert(key, header.1.parse().unwrap());
+            phase => {
+                panic!("Unsupported phase: {:?}", phase)
+            }
         }
-        let client = client.headers(headers);
-
-        let client = client.body(listen_response.body);
-        let http_response = client.send().await;
-
-        let mut http_response = match http_response {
-            Ok(v) => v,
-            Err(_) => todo!(),
-        };
-
-        let status_code = http_response.status().as_u16() as i32;
-
-        let headers = http_response.headers_mut();
-        let headers = headers
-            .iter_mut()
-            .map(|header| (header.0.to_string(), header.1.to_str().unwrap().to_string()))
-            .collect();
-
-        let body = http_response.bytes().await;
-        let body = match body {
-            Ok(v) => v,
-            Err(_) => todo!(),
-        };
-        let body = body.to_vec();
-
-        let listen_request = proto::teleproxy::ListenRequest {
-            api_key: "".to_string(),
-            id: id.to_string(),
-            phase: ListenPhase::Tunneling as i32,
-            status_code,
-            headers,
-            body,
-        };
-
-        let _ = stream_tx.send(listen_request).await;
     }
 
     Ok(())
--- src/dto/phase.rs
+++ src/dto/phase.rs
@@ -1,5 +1,19 @@
+#[derive(Debug)]
 pub enum ListenPhase {
     Init = 0,
     Tunneling = 1,
     Heartbeat = 99,
+}
+
+impl TryFrom<i32> for ListenPhase {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            x if x == ListenPhase::Init as i32 => Ok(ListenPhase::Init),
+            x if x == ListenPhase::Tunneling as i32 => Ok(ListenPhase::Tunneling),
+            x if x == ListenPhase::Heartbeat as i32 => Ok(ListenPhase::Heartbeat),
+            _ => Err(()),
+        }
+    }
 }
