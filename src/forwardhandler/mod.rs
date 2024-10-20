9bd46dcfe7f8c875741bc26dc6fb3547a1293dc6
BeLeap <beleap@beleap.dev>
2024-10-20 23:33:23 +0900

    add comment

--- src/forwardhandler/mod.rs
+++ src/forwardhandler/mod.rs
@@ -5,8 +5,10 @@ use std::{
 
 use crate::dto;
 
-type Sender =
-    tokio::sync::mpsc::Sender<(dto::http_request::HttpRequest, tokio::sync::oneshot::Sender<dto::http_response::HttpResponse>)>;
+type Sender = tokio::sync::mpsc::Sender<(
+    dto::http_request::HttpRequest,
+    tokio::sync::oneshot::Sender<dto::http_response::HttpResponse>,
+)>;
 pub struct ForwardHandler {
     handlers: Arc<Mutex<HashMap<String, Sender>>>,
 }
@@ -19,11 +21,13 @@ impl ForwardHandler {
     }
 
     pub fn register_sender(&self, id: &String, sender: Sender) {
+        // TODO: remove unwrap
         let mut handlers = self.handlers.lock().unwrap();
         handlers.insert(id.to_string(), sender);
     }
 
     pub fn get_sender(&self, id: &String) -> Sender {
+        // TODO: remove unwrap
         let handlers = self.handlers.lock().unwrap();
         handlers.get(id).unwrap().clone()
     }
