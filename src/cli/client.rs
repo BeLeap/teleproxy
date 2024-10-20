ff1bbb7bf398e19d2b6ed052aa4958b4271ed594
BeLeap <beleap@beleap.dev>
2024-10-20 19:50:07 +0900

    add command

--- src/cli/client.rs
+++ src/cli/client.rs
@@ -22,21 +22,21 @@ pub struct ClientArgs {
 }
 
 pub fn handler(args: &ClientArgs) {
-    match &args.command {
-        None => {
-            tokio::runtime::Builder::new_multi_thread()
-                .enable_all()
-                .build()
-                .unwrap()
-                .block_on(async move {
-                    let args = args;
-                    let mut teleproxy_client =
-                        proto::teleproxy::teleproxy_client::TeleproxyClient::connect(
-                            args.proxy_address.clone(),
-                        )
-                        .await
-                        .unwrap();
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async move {
+            let args = args;
+            let mut teleproxy_client =
+                proto::teleproxy::teleproxy_client::TeleproxyClient::connect(
+                    args.proxy_address.clone(),
+                )
+                .await
+                .unwrap();
 
+            match &args.command {
+                None => {
                     let header_key = &args.header_key;
                     let header_value = &args.header_value;
                     let id = client::register(
@@ -61,18 +61,29 @@ pub fn handler(args: &ClientArgs) {
                     log::info!("clien deregister request with id: {}", id);
                     let _ = client::deregister(&mut teleproxy_client, args.api_key.to_string(), id)
                         .await;
-                });
-        }
-        Some(command) => match command.as_str() {
-            "dump" => {
-                todo!();
+                }
+                Some(command) => match command.as_str() {
+                    "dump" => {
+                        let resp = teleproxy_client
+                            .dump(tonic::Request::new(proto::teleproxy::DumpRequest {
+                                api_key: args.api_key.to_string(),
+                            }))
+                            .await;
+
+                        println!("{}", resp.unwrap().into_inner().dump);
+                    }
+                    "flush" => {
+                        teleproxy_client
+                            .flush(tonic::Request::new(proto::teleproxy::FlushRequest {
+                                api_key: args.api_key.to_string(),
+                            }))
+                            .await
+                            .unwrap();
+                    }
+                    _ => {
+                        panic!("Unsupported command.");
+                    }
+                },
             }
-            "flush" => {
-                todo!();
-            }
-            _ => {
-                todo!();
-            }
-        },
-    }
+        });
 }
